1faeb892a415c900ba56d9bcd9a48dae6fc6edb5
cwn0828 <635058492@qq.com>
2019-08-24 16:23:17 +0800

    lesson-2

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <template>
   <div id="app">
+    <!-- getters -->
     <p>App.vue:{{count}}</p>
-    <p>App.vue:{{todos}}</p>
-    <Count />
+    <p>{{completedTodos}}</p>
+    <p>{{completedTodosCount}}</p>
+    <p>{{getTodosByID(3)}}</p>
+    <!-- <Count /> -->
   </div>
 </template>
 
 <script>
 import Count from "./components/Count";
 // {} 解构
-import { mapState } from "vuex";
+import { mapState, mapGetters } from "vuex";
 export default {
   name: "app",
   components: {
     Count
   },
+  computed: mapGetters([
+    "count",
+    "completedTodos",
+    "completedTodosCount",
+    "getTodosByID"
+  ])
+  // computed: {
+  //   count() {
+  //     return this.$store.getters.count;
+  //   },
+  //   completedTodos() {
+  //     return this.$store.getters.completedTodos;
+  //   },
+  //   completedTodosCount() {
+  //     return this.$store.getters.completedTodosCount;
+  //   },
+  //   getTodosByID() {
+  //     return this.$store.getters.getTodosByID;
+  //   }
+  // }
   // 写法3
-  computed: mapState(["count", "todos"])
+  // computed: mapState(["count", "todos"])
   // computed:{
   //   // ... 范围解析符
   //   ...mapState(["count", "todos"]),
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { stat } from 'fs';
 
 Vue.use(Vuex)
 
@@ -9,12 +10,35 @@ export default new Vuex.Store({
     count: 0,
     todos: [
       { id: 1, title: "todo item 1", completed: false },
-      { id: 2, title: "todo item 2", completed: false },
-      { id: 3, title: "todo item 3", completed: false },
+      { id: 2, title: "todo item 2", completed: true },
+      { id: 3, title: "todo item 3", completed: true },
     ]
   },
+  // 专门获取属性的对象
   getters: {
-
+    count: state => ++state.count,
+    // count(state) {
+    //   return ++state.count;
+    // }
+    completedTodos: state => state.todos.filter(todo => todo.completed),
+    // completedTodos: function (state) {
+    //   return state.todos.filter(function (todo) {
+    //     return todo.completed
+    //   })
+    // }
+    // 返回completed为真的对象的个数
+    completedTodosCount: (state, getters) => getters.completedTodos.length,
+    // completedTodosCount: function (state, getters) {
+    //   return getters.completedTodos.length;
+    // }
+    getTodosByID: state => id => state.todos.find(todo => todo.id == id)
+    // getTodosByID: function (state) {
+    //   (function (id) {
+    //     return state.todos.find(function (todo) {
+    //       return todo.id == id;
+    //     })
+    //   })(id)
+    // }
   },
   mutations: {
 
